480aaa5bf88134815a9cb4c5e3d6605ae0310f42
hangyuan <yuanhang3260@gmail.com>
2018-01-09 00:19:22 -0800

    fixed login page search box

--- WEB-INF/src/servlet/LoginServlet.java
+++ WEB-INF/src/servlet/LoginServlet.java
@@ -9,31 +9,31 @@ public class LoginServlet extends HttpServlet {
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    // response.setContentType("application/json; charset=utf-8");
-    // String username = request.getParameter("username");
-    // String password = request.getParameter("password");
-    // String rememberme = request.getParameter("rememberme");
-    // JSONObject obj = new JSONObject();
-    // obj.put("username", username);
-    // obj.put("password", password);
-    // obj.put("rememberme", rememberme);
+    response.setContentType("application/json; charset=utf-8");
+    String username = request.getParameter("username");
+    String password = request.getParameter("password");
+    String rememberme = request.getParameter("rememberme");
+    JSONObject obj = new JSONObject();
+    obj.put("username", username);
+    obj.put("password", password);
+    obj.put("rememberme", rememberme);
 
-    // PrintWriter out = null;
-    // try {
-    //   out = response.getWriter();
-    //   out.write(obj.toString());
-    // } catch (IOException e) {
-    //   e.printStackTrace();
-    // } finally {
-    //   if (out != null) {
-    //     out.close();
-    //   }
-    // }
+    PrintWriter out = null;
+    try {
+      out = response.getWriter();
+      out.write(obj.toString());
+    } catch (IOException e) {
+      e.printStackTrace();
+    } finally {
+      if (out != null) {
+        out.close();
+      }
+    }
     request.getSession();
 
-    String redirect_to = "test.html";
-    RequestDispatcher d = request.getRequestDispatcher(redirect_to);
-    d.forward(request, response);
+    // String redirect_to = "test.html";
+    // RequestDispatcher d = request.getRequestDispatcher(redirect_to);
+    // d.forward(request, response);
     return;
   }
 }
