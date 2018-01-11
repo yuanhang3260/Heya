b874b5743ae4c56ca3d0861ba225ff5c6ac3de7b
hangyuan <yuanhang3260@gmail.com>
2018-01-11 01:28:05 -0800

    add signup link to index.html

--- js/login.js
+++ js/login.js
@@ -4,31 +4,14 @@ define(["jquery"], function($) {
     this.username_input = this.el.find("input[name=username]");
     this.password_input = this.el.find("input[name=password]");
 
-    this.username_input_ever = false;
-
     this.el.keyup($.proxy(this.checkInputNonEmpty, this));
-
-    this.username_input.focusout(
-        $.proxy(this.checkUserNameNonEmpty, this));
-    this.username_input.focus(
-        $.proxy(this.restoreUsernameInput, this));
   };
 
   Login.prototype.checkInputNonEmpty = function() {
-    if (this.username_input.val()) {
-      this.username_input_ever = true;
-      if (this.password_input.val()) {
-        this.enable();
-        return;
-      }
-    }
-    this.disable();
-  };
-
-  Login.prototype.checkUserNameNonEmpty = function() {
-    if (this.username_input_ever && !this.username_input.val()) {
-      // this.username_input.attr("placeholder", "Error");
-      this.username_input.addClass("inputbox-error");
+    if (this.username_input.val() && this.password_input.val()) {
+      this.enable();
+    } else {
+      this.disable();
     }
   };
 
@@ -40,11 +23,6 @@ define(["jquery"], function($) {
   Login.prototype.enable = function() {
     this.el.find("button[type=submit]").removeAttr("disabled");
     this.el.find("input[name=rememberme]").removeAttr("disabled");
-  }
-
-  Login.prototype.restoreUsernameInput = function() {
-    // this.username_input.attr("placeholder", "Username");
-    this.username_input.removeClass("inputbox-error");
   }
 
   return {
